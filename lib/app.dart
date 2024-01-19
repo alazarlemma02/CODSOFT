48bd1b83149c2ac77cb4d8393541dd3c18299ddb
alazarlemma02 <alazar03lemma@gmail.com>
2024-01-20 01:09:29 +0300

    created to be the first screen

---
+++ lib/app.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Main App"),
+        centerTitle: true,
+      ),
+    );
+  }
+}
