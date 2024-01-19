2450e224cdd01931f0c21bdc856e90b42108742a
alazarlemma02 <alazar03lemma@gmail.com>
2024-01-20 01:08:36 +0300

    instantiated the database and table creation process

---
+++ lib/data/services/database_provider.dart
@@ -0,0 +1,35 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class DatabaseProvider {
+  static final DatabaseProvider instance = DatabaseProvider._init();
+  static Database? _database;
+  DatabaseProvider._init();
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDB('tasks.db');
+    return _database!;
+  }
+
+  Future<Database> _initDB(String filePath) async {
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, filePath);
+    return await openDatabase(path, version: 1, onCreate: _createDB);
+  }
+
+  Future _createDB(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE tasks(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT NOT NULL,
+        description TEXT  NOT NULL,
+        isCompleted INTEGER NOT NULL,
+        createdAt TEXT NOT NULL,
+        completedAt TEXT
+      )
+''');
+  }
+
+  // CRUD
+}
