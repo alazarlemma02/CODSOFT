99416d2f4f02da1f90f3bda65e5decb11831e2ae
alazarlemma02 <alazar03lemma@gmail.com>
2024-01-20 19:28:58 +0300

    created all the necessary states

--- lib/bloc/task_state.dart
+++ lib/bloc/task_state.dart
@@ -2,9 +2,43 @@ part of 'task_bloc.dart';
 
 sealed class TaskState extends Equatable {
   const TaskState();
-  
+
   @override
   List<Object> get props => [];
 }
 
-final class TaskInitial extends TaskState {}
+class TaskInitial extends TaskState {}
+
+class TaskActionState extends TaskState {}
+
+class TaskLoadingState extends TaskState {}
+
+class TaskLoadedState extends TaskState {
+  final List<Task> task;
+
+  const TaskLoadedState({required this.task});
+
+  @override
+  List<Object> get props => [task];
+}
+
+class TaskAddingState extends TaskState {}
+
+class TaskAddedActionState extends TaskActionState {}
+
+class TaskDeletingState extends TaskState {}
+
+class TaskDeletedActionState extends TaskActionState {}
+
+class TaskUpdatingState extends TaskState {}
+
+class TaskUpdatedActionState extends TaskActionState {}
+
+class TaskErrorState extends TaskState {
+  final String message;
+
+  const TaskErrorState({required this.message});
+
+  @override
+  List<Object> get props => [message];
+}
