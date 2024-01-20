4c76b0ce1854b7aeb479afe4ebe916cd8e5520ef
alazarlemma02 <alazar03lemma@gmail.com>
2024-01-20 19:29:26 +0300

    setted up necessary events for the bloc

--- lib/bloc/task_event.dart
+++ lib/bloc/task_event.dart
@@ -6,3 +6,41 @@ sealed class TaskEvent extends Equatable {
   @override
   List<Object> get props => [];
 }
+
+class TaskInitEvent extends TaskEvent {}
+
+class TaskAddEvent extends TaskEvent {
+  final Task task;
+
+  const TaskAddEvent({required this.task});
+
+  @override
+  List<Object> get props => [task];
+}
+
+class TaskUpdateEvent extends TaskEvent {
+  final Task task;
+
+  const TaskUpdateEvent({required this.task});
+
+  @override
+  List<Object> get props => [task];
+}
+
+class TaskDeleteEvent extends TaskEvent {
+  final Task task;
+
+  const TaskDeleteEvent({required this.task});
+
+  @override
+  List<Object> get props => [task];
+}
+
+class TaskCompleteEvent extends TaskEvent {
+  final Task task;
+
+  const TaskCompleteEvent({required this.task});
+
+  @override
+  List<Object> get props => [task];
+}
