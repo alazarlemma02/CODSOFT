ce92d8c5c8171c748a8f3f8b3b2a15ed9f16afcf
alazarlemma02 <alazar03lemma@gmail.com>
2024-01-23 22:36:56 +0300

    cerated a welcome screen if initial using the app

---
+++ lib/screens/views/welcome_screen.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:task_app/bloc/welcome_bloc/welcome_bloc.dart';
+import 'package:task_app/screens/widgets/welcome_screen_widget.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  State<WelcomeScreen> createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  final PageController pageController = PageController();
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Image.asset("assets/images/welcome_background.png"),
+        Scaffold(
+          backgroundColor: Colors.transparent,
+          body: BlocBuilder<WelcomeBloc, WelcomeInitial>(
+            builder: (context, state) {
+              return Stack(
+                alignment: FractionalOffset.bottomCenter,
+                children: [
+                  PageView(
+                    controller: pageController,
+                    onPageChanged: (value) {
+                      state.pageIndex = value;
+                      BlocProvider.of<WelcomeBloc>(context)
+                          .add(WelcomePageChangedEvent());
+                    },
+                    children: [
+                      WelcomeScreenWidget(
+                        pageController: pageController,
+                        pageIndex: 0,
+                        title: "Welcome to TaskMaster!",
+                        subtitle: "Let's make today super productive together!",
+                        buttonTitle: "Let's Start",
+                        imageUrl: "assets/images/welcome_logo.png",
+                        context: context,
+                      ),
+                    ],
+                  ),
+                ],
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
