04c8cf6565a2aa8dfbbc2deb27fe7dacb92ce940
alazarlemma02 <alazar03lemma@gmail.com>
2024-01-27 15:04:21 +0300

    added state management based on the state

--- lib/screens/widgets/common_task_widget.dart
+++ lib/screens/widgets/common_task_widget.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:task_app/bloc/task_bloc/task_bloc.dart';
+import 'package:task_app/config/theme.dart';
 import 'package:task_app/data/model/task.dart';
 
 class CommonTaskWidget extends StatefulWidget {
@@ -9,12 +10,14 @@ class CommonTaskWidget extends StatefulWidget {
   final List<Task> task;
   final String title;
   final bool isCompleted;
-  const CommonTaskWidget(
-      {super.key,
-      required this.taskBloc,
-      required this.task,
-      required this.title,
-      required this.isCompleted});
+
+  const CommonTaskWidget({
+    Key? key,
+    required this.taskBloc,
+    required this.task,
+    required this.title,
+    required this.isCompleted,
+  }) : super(key: key);
 
   @override
   State<CommonTaskWidget> createState() => _CommonTaskWidgetState();
@@ -24,119 +27,173 @@ class _CommonTaskWidgetState extends State<CommonTaskWidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: BlocConsumer<TaskBloc, TaskState>(
-      listener: (context, state) {},
-      builder: (context, state) {
-        if (state is TaskLoadingState) {
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
-        } else if (state is TaskCompletedLoadedState ||
-            state is TaskPendingLoadedState) {
-          final tasks = (state is TaskCompletedLoadedState)
-              ? (state).tasks
-              : (state as TaskPendingLoadedState).tasks;
-
-          if (tasks.isEmpty) {
-            return Center(
-              child: Padding(
-                padding:
-                    EdgeInsets.all(MediaQuery.of(context).size.width * 0.1),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Image(
-                      image: AssetImage("assets/images/no_task.png"),
-                    ),
-                    Text(
-                      widget.title,
-                      style: const TextStyle(
-                        fontSize: 18,
-                        color: Colors.grey,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
+      body: BlocConsumer<TaskBloc, TaskState>(
+        listener: (context, state) {},
+        builder: (context, state) {
+          if (state is TaskLoadingState) {
+            return const Center(
+              child: CircularProgressIndicator(),
             );
-          } else {
-            return ListView.builder(
-              itemCount: tasks.length,
-              itemBuilder: (context, index) {
-                final task = tasks[index];
-                final descriptionPreview = task.description.length > 50
-                    ? '${task.description.substring(0, 50)}...'
-                    : task.description;
-                return Slidable(
-                  startActionPane: ActionPane(
-                    motion: const BehindMotion(),
+          } else if (state is TaskCompletedLoadedState ||
+              state is TaskPendingLoadedState) {
+            final tasks = (state is TaskCompletedLoadedState)
+                ? (state).tasks
+                : (state as TaskPendingLoadedState).tasks;
+
+            if (tasks.isEmpty) {
+              return Center(
+                child: Padding(
+                  padding:
+                      EdgeInsets.all(MediaQuery.of(context).size.width * 0.1),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      SlidableAction(
-                          icon: Icons.check_circle,
-                          backgroundColor: Colors.green,
-                          label: "Complete",
-                          onPressed: (context) {
-                            widget.taskBloc.add(TaskCompleteEvent(task: task));
-                          })
-                    ],
-                  ),
-                  endActionPane: ActionPane(
-                    motion: const BehindMotion(),
-                    children: [
-                      SlidableAction(
-                        icon: Icons.delete,
-                        label: "Remove",
-                        backgroundColor: Colors.red,
-                        onPressed: (context) {
-                          widget.taskBloc.add(TaskDeleteEvent(task: task));
-                        },
+                      Image(
+                        image: const AssetImage("assets/images/no_task.png"),
+                        color: lightColorScheme.tertiary,
                       ),
-                    ],
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: ListTile(
-                      onTap: () {},
-                      title: Text(
-                        task.title,
+                      Text(
+                        widget.title,
                         style: const TextStyle(
-                          fontSize: 18.0,
-                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                          color: Colors.grey,
                         ),
                       ),
-                      subtitle: Text(
-                        descriptionPreview,
-                        style: TextStyle(
-                          color: Colors.grey[600],
+                    ],
+                  ),
+                ),
+              );
+            } else {
+              return ListView.builder(
+                itemCount: tasks.length,
+                itemBuilder: (context, index) {
+                  final task = tasks[index];
+                  final descriptionPreview = task.description.length > 50
+                      ? '${task.description.substring(0, 50)}...'
+                      : task.description;
+                  return Slidable(
+                    startActionPane: task.isCompleted
+                        ? null
+                        : ActionPane(
+                            motion: const BehindMotion(),
+                            children: [
+                              SlidableAction(
+                                icon: Icons.check_circle,
+                                backgroundColor: Colors.green,
+                                label: "Complete",
+                                onPressed: (context) {
+                                  widget.taskBloc
+                                      .add(TaskCompleteEvent(task: task));
+                                },
+                              )
+                            ],
+                          ),
+                    endActionPane: ActionPane(
+                      motion: const BehindMotion(),
+                      children: [
+                        SlidableAction(
+                          icon: Icons.delete,
+                          label: "Remove",
+                          backgroundColor: Colors.red,
+                          onPressed: (context) {
+                            widget.taskBloc.add(TaskDeleteEvent(task: task));
+                          },
                         ),
-                      ),
-                      trailing: Checkbox(
-                        value: task.isCompleted,
-                        onChanged: (value) {
-                          if (state is! TaskCompletedLoadedState) {
-                            widget.taskBloc.add(TaskCompleteEvent(task: task));
-                          }
-                        },
-                        activeColor: Colors.green,
-                      ),
-                      tileColor: task.isCompleted
-                          ? Colors.green[100]
-                          : Colors.grey[200],
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
+                      ],
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        margin: const EdgeInsets.symmetric(vertical: 8.0),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20.0),
+                          border: Border.all(
+                            color: Theme.of(context).dividerColor,
+                            width: 1.0,
+                          ),
+                        ),
+                        child: ExpansionTile(
+                          title: Text(
+                            task.title,
+                            style: TextStyle(
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.bold,
+                              color: lightColorScheme.primary,
+                            ),
+                          ),
+                          subtitle: Text(
+                            descriptionPreview,
+                            style: TextStyle(
+                              color: Colors.grey[600],
+                            ),
+                          ),
+                          tilePadding:
+                              const EdgeInsets.symmetric(horizontal: 16.0),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          children: [
+                            ListTile(
+                              title: Text(
+                                'Description: ${task.description}',
+                                style: TextStyle(
+                                  color: Colors.grey[600],
+                                ),
+                              ),
+                            ),
+                            ListTile(
+                              title: Text(
+                                'Start Time: ${task.startTime.format(context)}',
+                                style: TextStyle(
+                                  color: Colors.grey[600],
+                                ),
+                              ),
+                            ),
+                            ListTile(
+                              title: Row(
+                                children: [
+                                  Text(
+                                    'End Time: ${task.endTime.format(context)}',
+                                    style: TextStyle(
+                                      color: Colors.grey[600],
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    width: MediaQuery.of(context).size.width *
+                                        0.15,
+                                  ),
+                                  ElevatedButton(
+                                      onPressed: () {
+                                        if (!task.isCompleted) {
+                                          widget.taskBloc.add(
+                                              TaskCompleteEvent(task: task));
+                                          BlocProvider.of<TaskBloc>(context)
+                                              .add(const TaskFilterEvent(
+                                                  filterKey: 'isCompleted',
+                                                  status: false));
+                                        }
+                                      },
+                                      child: task.isCompleted
+                                          ? const Text("Done")
+                                          : const Text("Complete")),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-                );
-              },
+                  );
+                },
+              );
+            }
+          } else {
+            return const Center(
+              child: Text("error..."),
             );
           }
-        } else {
-          return const Center(
-            child: Text("error..."),
-          );
-        }
-      },
-    ));
+        },
+      ),
+    );
   }
 }
