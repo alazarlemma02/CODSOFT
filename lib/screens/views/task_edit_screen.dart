6d7d57137b0c5a9601a7891a19e30438adb9b275
alazarlemma02 <alazar03lemma@gmail.com>
2024-01-27 15:03:15 +0300

    added functionality to update a task

--- lib/screens/views/task_edit_screen.dart
+++ lib/screens/views/task_edit_screen.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:task_app/bloc/task_bloc/task_bloc.dart';
+import 'package:task_app/config/field_validator.dart';
+import 'package:task_app/config/theme.dart';
 import 'package:task_app/data/model/task.dart';
-import 'package:task_app/screens/widgets/dark_light_mode.dart';
 import 'package:task_app/screens/widgets/drawer_widget.dart';
 
 class TaskEditScreen extends StatefulWidget {
   final Task task;
-  const TaskEditScreen({super.key, required this.task});
+  const TaskEditScreen({Key? key, required this.task}) : super(key: key);
 
   @override
   State<TaskEditScreen> createState() => _TaskEditScreenState();
@@ -16,6 +17,56 @@ class TaskEditScreen extends StatefulWidget {
 class _TaskEditScreenState extends State<TaskEditScreen> {
   final TextEditingController _titleController = TextEditingController();
   final TextEditingController _descriptionController = TextEditingController();
+  DateTime _selectedDate = DateTime.now();
+  TimeOfDay _selectedStartTime = TimeOfDay.now();
+  TimeOfDay _selectedEndTime =
+      TimeOfDay.fromDateTime(DateTime.now().add(const Duration(hours: 2)));
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    super.initState();
+    _titleController.text = widget.task.title;
+    _descriptionController.text = widget.task.description;
+    _selectedDate = widget.task.createdAt;
+    _selectedStartTime = widget.task.startTime;
+    _selectedEndTime = widget.task.endTime;
+  }
+
+  Future<void> _showDatePicker() async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: _selectedDate,
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2100),
+    );
+
+    if (picked != null && picked != _selectedDate) {
+      setState(() {
+        _selectedDate = picked;
+      });
+    }
+  }
+
+  Future<void> _showTimePicker(bool isStartTime) async {
+    final TimeOfDay? picked = await showTimePicker(
+      context: context,
+      initialTime: isStartTime ? _selectedStartTime : _selectedEndTime,
+    );
+
+    if (picked != null &&
+        (isStartTime
+            ? picked != _selectedStartTime
+            : picked != _selectedEndTime)) {
+      setState(() {
+        if (isStartTime) {
+          _selectedStartTime = picked;
+        } else {
+          _selectedEndTime = picked;
+        }
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,66 +75,187 @@ class _TaskEditScreenState extends State<TaskEditScreen> {
       appBar: AppBar(
         title: const Text("Edit Task"),
         centerTitle: true,
-        actions: const [
-          DarkLightMode(),
-        ],
       ),
-      body: Column(
-        children: [
-          TextFormField(
-            controller: _titleController,
-            decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(15),
-                ),
-                filled: true,
-                fillColor: Colors.grey[200]),
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          TextFormField(
-            controller: _descriptionController,
-            decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(15),
-                ),
-                filled: true,
-                fillColor: Colors.grey[200]),
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          Row(
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(backgroundColor: Colors.white),
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: const Text(
-                  "Cancel",
-                  style: TextStyle(color: Colors.red),
+              TextFormField(
+                controller: _titleController,
+                decoration: InputDecoration(
+                  labelText: "Task Name",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
                 ),
+                validator: (value) =>
+                    Validator.validateTitle(title: _titleController.text),
               ),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(backgroundColor: Colors.white),
-                onPressed: () {
-                  final updatedTask = widget.task.copyWith(
-                      title: _titleController.text,
-                      description: _descriptionController.text);
-                  BlocProvider.of<TaskBloc>(context)
-                      .add(TaskUpdateEvent(task: updatedTask));
-                },
-                child: const Text(
-                  "Update",
-                  style: TextStyle(color: Colors.green),
+              const SizedBox(height: 16),
+              TextFormField(
+                controller: _descriptionController,
+                maxLines: 3,
+                decoration: InputDecoration(
+                  labelText: "Description",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
                 ),
+                validator: (value) => Validator.validateDescription(
+                    description: _descriptionController.text),
+              ),
+              const SizedBox(height: 16),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Row(
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            "Date",
+                            style: TextStyle(fontSize: 16),
+                          ),
+                          const SizedBox(height: 8),
+                          GestureDetector(
+                            onTap: _showDatePicker,
+                            child: Container(
+                              padding: EdgeInsets.all(
+                                  MediaQuery.of(context).size.width * 0.03),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                border: Border.all(color: Colors.grey),
+                              ),
+                              child: Center(
+                                child: Text(
+                                  "${_selectedDate.toLocal()}".split(' ')[0],
+                                  style: const TextStyle(fontSize: 16),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 16),
+                  Row(
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            "Start Time",
+                            style: TextStyle(fontSize: 16),
+                          ),
+                          const SizedBox(height: 8),
+                          GestureDetector(
+                            onTap: () => _showTimePicker(true),
+                            child: Container(
+                              padding: EdgeInsets.all(
+                                  MediaQuery.of(context).size.width * 0.03),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                border: Border.all(color: Colors.grey),
+                              ),
+                              child: Center(
+                                child: Text(
+                                  _selectedStartTime.format(context).toString(),
+                                  style: const TextStyle(fontSize: 16),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 16),
+                  Row(
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            "End Time",
+                            style: TextStyle(fontSize: 16),
+                          ),
+                          const SizedBox(height: 8),
+                          GestureDetector(
+                            onTap: () => _showTimePicker(false),
+                            child: Container(
+                              padding: EdgeInsets.all(
+                                  MediaQuery.of(context).size.width * 0.03),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                border: Border.all(color: Colors.grey),
+                              ),
+                              child: Center(
+                                child: Text(
+                                  _selectedEndTime.format(context).toString(),
+                                  style: const TextStyle(fontSize: 16),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              const SizedBox(height: 16),
+              Row(
+                children: [
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.red,
+                    ),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text(
+                      "Cancel",
+                      style: TextStyle(color: lightColorScheme.shadow),
+                    ),
+                  ),
+                  const SizedBox(width: 16),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.green,
+                    ),
+                    onPressed: () {
+                      _updateTodo(context);
+                      Navigator.pop(context, true);
+                    },
+                    child: Text(
+                      "Update",
+                      style: TextStyle(color: lightColorScheme.shadow),
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
-        ],
+        ),
       ),
     );
+  }
+
+  void _updateTodo(BuildContext context) {
+    if (_formKey.currentState!.validate()) {
+      BlocProvider.of<TaskBloc>(context).add(TaskUpdateEvent(
+          task: widget.task.copyWith(
+        title: _titleController.text,
+        description: _descriptionController.text,
+        createdAt: _selectedDate,
+        startTime: _selectedStartTime,
+        endTime: _selectedEndTime,
+      )));
+    }
   }
 }
