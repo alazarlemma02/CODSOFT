a998adf6f1829e9dc53e9dc12cde9ba69402fff1
alazarlemma02 <alazar03lemma@gmail.com>
2024-01-27 15:01:29 +0300

    added additional modifcation and managed state

--- lib/screens/views/completed_screen.dart
+++ lib/screens/views/completed_screen.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:task_app/bloc/task_bloc/task_bloc.dart';
+import 'package:task_app/config/theme.dart';
 import 'package:task_app/screens/widgets/common_task_widget.dart';
-import 'package:task_app/screens/widgets/dark_light_mode.dart';
 import 'package:task_app/screens/widgets/drawer_widget.dart';
+import 'package:task_app/screens/widgets/pop_up_menu.dart';
 
 class CompletedScreen extends StatefulWidget {
   const CompletedScreen({super.key});
@@ -26,28 +27,46 @@ class _CompletedScreenState extends State<CompletedScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       drawer: const DrawerWidget(),
-      appBar: AppBar(
-        title: const Text("Completed Todo"),
-        centerTitle: true,
-        actions: const [DarkLightMode()],
-      ),
-      body: BlocBuilder<TaskBloc, TaskState>(
-        builder: (context, state) {
-          if (state is TaskCompletedLoadedState) {
-            return CommonTaskWidget(
-              taskBloc: taskBloc,
-              isCompleted: true,
-              task: state.tasks,
-              title: "You have Completed all Tasks!",
-            );
-          } else {
-            return const Center(
-              child: CircularProgressIndicator(
-                color: Colors.green,
-              ),
-            );
-          }
-        },
+      body: NestedScrollView(
+        headerSliverBuilder: (context, innerBoxIsScrolled) => [
+          SliverAppBar(
+            title: const Text("Completed Todo"),
+            centerTitle: true,
+            actions: [PopUpMenu(taskBloc: taskBloc, pageIndex: 2)],
+            floating: true,
+            snap: true,
+          ),
+        ],
+        body: BlocConsumer<TaskBloc, TaskState>(
+          listenWhen: (previous, current) => current is TaskActionState,
+          buildWhen: (previous, current) => current is! TaskActionState,
+          listener: (context, state) {
+            if (state is TaskDeletedActionState) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(
+                  content: Text("Tasks Deleted Successfully!"),
+                  backgroundColor: Colors.red,
+                ),
+              );
+            }
+          },
+          builder: (context, state) {
+            if (state is TaskCompletedLoadedState) {
+              return CommonTaskWidget(
+                taskBloc: taskBloc,
+                isCompleted: true,
+                task: state.tasks,
+                title: "You have Completed all Tasks!",
+              );
+            } else {
+              return Center(
+                child: CircularProgressIndicator(
+                  color: lightColorScheme.primary,
+                ),
+              );
+            }
+          },
+        ),
       ),
     );
   }
