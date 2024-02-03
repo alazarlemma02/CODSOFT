c0b7bea5671353fcf07a360f23241adb146093dc
alazarlemma02 <alazar03lemma@gmail.com>
2024-02-03 19:08:55 +0300

    setted up list of images for the background of the quotes

---
+++ lib/data/services/background_images.dart
@@ -0,0 +1,8 @@
+List<String> backgroundImages = [
+  "https://i.pinimg.com/736x/23/1f/1b/231f1bc7829ad3f12a7c965d066df03b.jpg",
+  "https://ofhsoupkitchen.org/wp-content/uploads/2022/11/inspirational-one-words.jpg",
+  "https://wallpaper.dog/large/984083.jpg",
+  "https://wallpaper.dog/large/10721339.jpg",
+  "https://wallpaper.dog/large/10721342.jpg",
+  "https://wallpaper.dog/large/10721421.jpg"
+];
