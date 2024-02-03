ec62efadf972b8695d7b06ac8378e54cc77611bf
alazarlemma02 <alazar03lemma@gmail.com>
2024-02-03 19:08:25 +0300

    modified the main screen and integrated with bloc

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:daily_quote/bloc/quote_bloc/quote_bloc.dart';
+import 'package:daily_quote/screens/views/quote_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<QuoteBloc>(
+          create: (context) => QuoteBloc(),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const QuoteScreen(),
+      ),
+    );
+  }
+}
